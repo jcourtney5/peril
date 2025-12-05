e231fb589d595c6a74cf0c961bdd1892a84f163e
jcourtney5 <jesse.courtney5@gmail.com>
2025-12-04 22:35:03 -0700

    Added RabbitMQ connecting code to server and handling Ctrl+C to exit

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+	"os/signal"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+)
 
 func main() {
-	fmt.Println("Starting Peril server...")
+	// connect to RabbitMQ
+	const rabbitMqConnection = "amqp://guest:guest@localhost:5672/"
+	conn, err := amqp.Dial(rabbitMqConnection)
+	if err != nil {
+		log.Fatalf("Error connecting to RabbitMq: %v", err)
+	}
+	defer conn.Close()
+
+	fmt.Println("Peril game server started and connected to RabbitMQ")
+
+	// wait for Ctrl+C to exit program
+	signalChan := make(chan os.Signal, 1)
+	signal.Notify(signalChan, os.Interrupt)
+	fmt.Println("Server running, press Ctrl+C to stop...")
+	<-signalChan // Will block here until user hits ctrl+c
+	fmt.Println("Stopping server and disconnecting from RabbitMQ...")
 }
