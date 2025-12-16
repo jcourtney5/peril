c1e61ae9bc867b9806dc1dce0df9bf930a987959
jcourtney5 <jesse.courtney5@gmail.com>
2025-12-15 22:17:17 -0700

    added Qos call before consuming from a queue and limit prefetch to 10

--- internal/pubsub/consume.go
+++ internal/pubsub/consume.go
@@ -83,6 +83,11 @@ func subscribe[T any](
 		return fmt.Errorf("could not declare and bind queue: %v", err)
 	}
 
+	err = ch.Qos(10, 0, true)
+	if err != nil {
+		return fmt.Errorf("could not set QoS: %v", err)
+	}
+
 	msgs, err := ch.Consume(
 		queue.Name,
 		"",
